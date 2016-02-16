c9c6ae4d7d93569e809e46dde5b839f139d84e19
Cody Bouscaren <codybousc@gmail.com>
2016-02-16 17:45:14 -0500

    FaceBook likes

--- index.js
+++ index.js
@@ -61,3 +61,49 @@ Test.describe('', function() {
     Test.assertEquals(f(15, 10), 60);
   });
 });
+
+//Facebook Likes
+
+function likes(names) {
+     var singularText = " likes this";
+     var pluralText = " like this";
+     if(names.length == 0) {
+       return "no one" + singularText;
+     }
+     for(var i = 0; i < names.length; i++) {
+       if(names.length == 1) {
+         return names[i] + singularText;
+       }
+       if(names.length == 2) {
+         return names[i] + " and " + names[i + 1] + pluralText;
+       }
+       if(names.length == 3) {
+         return names[i] + ", " + names[i + 1] + " and " + names[i + 2] + pluralText;
+       }
+       if (names.length > 3) {
+         var totalOthers = names.length - 2;
+         return names[i] + ", " + names[i + 1] + " and " + totalOthers + " others like this"
+       }
+     }
+   }
+
+
+
+   console.log(likes([]), 'no one likes this');
+   console.log(likes(['Peter']), 'Peter likes this');
+   console.log(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+   console.log(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+   console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+
+  //Facebook likes top chosen
+  //======================================================================================
+  function likes(names) {
+  names = names || [];
+  switch(names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return names[0] + ' likes this'; break;
+    case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+  }
+}
